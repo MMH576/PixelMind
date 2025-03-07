31e3e6f2721899d89212c5bbd1eb55afbc65c94b
MMH576 <monjorulmohammed@gmail.com>
2025-03-07 12:36:38 -0500

    added base code on createpost.jsx

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -1,16 +1,60 @@
 import React, { useState } from "react";
-import { Loader } from "../components";
 import { useNavigate } from "react-router-dom";
 import { preview } from "../assets";
 import { FormField, Loader, Card } from "../components";
 import { getRandomPrompt } from "../utils";
 
 const CreatePost = () => {
+  const navigate = useNavigate();
   const [form, setForm] = useState({
     name: "",
     prompt: "",
     photo: "",
   });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {
+    console.log(form);
+  };
+
+  const handleChange = (e) => {};
+
+  const handleSurpriseMe = () => {};
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="text-4xl font-extrabold text-white">Create</h1>
+        <p className="mt-2 text-gray-600">
+          Create imaginative and visually stunning images through AI and share
+          them with the community
+        </p>
+      </div>
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            labelName="Your name"
+            type="text"
+            name="name"
+            placeholder="Ex. John Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName="Your name"
+            type="text"
+            name="prompt"
+            placeholder="A fantasy landscape with a castle and a river"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default CreatePost;
