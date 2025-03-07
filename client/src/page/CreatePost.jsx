758419f49eaff0934531ac7a78ea9df9eea46573
MMH576 <monjorulmohammed@gmail.com>
2025-03-07 12:08:40 -0500

    created get random promt in utils

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { Loader } from "../components";
+import { useNavigate } from "react-router-dom";
+import { preview } from "../assets";
+import { FormField, Loader, Card } from "../components";
+import { getRandomPrompt } from "../utils";
 
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  const [form, setForm] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  });
 };
 
 export default CreatePost;
